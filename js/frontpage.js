13528294651791c5a359fae6a7bf2254fbca7ee4
Roger <rgrang816@west-mec.org>
2022-02-15 15:58:24 -0700

    working on project menu

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,6 +1,6 @@
-$('document').ready(function(){
-    $("#hideButton").on("click",function(){
-        $("#top_nav").slideToggle(0);
-        $("#hideButton").text($("#hideButton").text()=="Hide"?"Show":"Hide")
+$('document').ready(function () {
+    $("#hideButton").on("click", function () {
+        $("#top_nav").slideToggle(1000)
+        $("#hideButton").text($("#hideButton").text() == "Hide" ? "Show" : "Hide")
     })
 })
